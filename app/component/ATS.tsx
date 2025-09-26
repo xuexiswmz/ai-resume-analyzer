c0900a36e8d7a6e4b5184b0dc1481273c46b3a3d
chenyixiang <chenyixiang@baimaohui.net>
2025-09-26 16:58:49 +0800

    feat: ResumeFeedbackPage

---
+++ app/component/ATS.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ATS = () => {
+    return (
+        <div>ATS</div>
+    )
+}
+export default ATS
--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ export default [
     index("routes/home.tsx"),
     route('/auth', 'routes/auth.tsx'),
     route('/upload', 'routes/upload.tsx'),
+    route('/resume', 'routes/resume.tsx'),
 ] satisfies RouteConfig;
