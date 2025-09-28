44c4b867191f5b6def94b1cdf7a9e2bbc5af6963
chenyixiang <chenyixiang@baimaohui.net>
2025-09-28 09:28:17 +0800

    fix: resumeFeedbackPage路由问题

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,5 @@ export default [
     index("routes/home.tsx"),
     route('/auth', 'routes/auth.tsx'),
     route('/upload', 'routes/upload.tsx'),
-    route('/resume', 'routes/resume.tsx'),
+    route('/resume/:id', 'routes/resume.tsx'),
 ] satisfies RouteConfig;
